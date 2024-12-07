739266d9bbeca082281134b204d04e43c0e07387
yhyuntak <yhyuntak@gmail.com>
2024-12-07 15:10:20 +0900

    [BUGFIX] tailwindcss 세팅 건

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="flex flex-row justify-between items-center h-[100px] bg-amber-600">
+    <div className="flex justify-between items-center h-[100px] bg-amber-600">
       <div className="text-2xl">뽀또의 개발 일지</div>
       <div className="flex gap-3.5">
         <div>검색</div>
